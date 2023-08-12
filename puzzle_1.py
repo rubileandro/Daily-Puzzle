491a0101ea46da8174421fdc6606e3ae324bf8d2
Rubi Leandro <rubi@brislandventures.com>
2023-08-12 17:32:28 +0400

    first commit

---
+++ puzzle_1.py
@@ -0,0 +1,18 @@
+# Probelm
+'''
+A salesman sold twice as much pears in the afternoon than in the morning.
+If he sold 360 kilograms of pears that day, how many kilograms did he sell in the morning 
+and how many in the afternoon?
+'''
+
+total_kg = 360
+
+am_sales = total_kg / 3
+pm_sales = 2 * am_sales
+
+print(f"Morning sales: {am_sales}")
+print(f"Afternoon sales: {pm_sales}")
+
+
+# Source
+https://www.math10.com/en/algebra/word-problems.html

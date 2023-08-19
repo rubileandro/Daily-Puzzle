0ecedf0122126d8b4fc3ab2aa15bd9cce326cf8e
Rubi Leandro <rubi@brislandventures.com>
2023-08-19 16:59:46 +0400

    puzzle 2

---
+++ puzzle2.py
@@ -0,0 +1,13 @@
+"""
+In Science class, Sara needed 8 test tubes for 3 different experiments. 
+The first experiment required 2 test tubes and the other two experiments required the same number of test tubes. 
+How many test tubes were needed for each of the other two experiments?
+"""
+total_tubes = 8
+experiments = 3
+
+tubes_exp1 = 2
+
+tubes_exp23 = (total_tubes - tubes_exp1) / 2
+
+print(tubes_exp23)
